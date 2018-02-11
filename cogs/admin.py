b22dea64372e29c5a0abd062d4071d384a445cbd
Tommy Ingdal <tommy.ingdal@gmail.com>
2018-02-11 18:51:30 +0100

    Added a command for fetching the source code for a specific command

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,13 +1,55 @@
 import traceback
 import inspect
 import discord
+import os
+import logging
+import sys
 from discord.ext import commands
 from utils import checks
+
+log = logging.getLogger(__name__)
 
 
 class Admin:
     def __init__(self, tweety):
         self.bot = tweety
+
+    @commands.command()
+    async def source(self, ctx, command: str=None):
+        """Display the source code of a given command"""
+        source_url = 'https://github.com/s1gh/tweety'
+        if command is None:
+            await ctx.send(source_url)
+            return
+
+        obj = self.bot.get_command(command.replace('.', ' '))
+        src = obj.callback.__code__
+        lines, firstlineno = inspect.getsourcelines(src)
+
+        if not obj.callback.__module__.startswith('discord'):
+            # not a built-in command
+            location = os.path.relpath(src.co_filename).replace('\\', '/')
+        else:
+            location = obj.callback.__module__.replace('.', '/') + '.py'
+            source_url = 'https://github.com/Rapptz/discord.py'
+
+        final_url = '<{}/blob/master/{}#L{}-L{}>'.format(source_url, location, firstlineno,
+                                                         firstlineno + len(lines) - 1)
+
+        if len(lines) < 30:
+            lines = [x.replace('```', '´´´') for x in lines]
+            await ctx.send('**Source Code: {}**\n```python\n{}\n```'.format(command, ' '.join(lines)))
+        else:
+            await ctx.send(final_url)
+
+    @commands.command(hidden=True)
+    @checks.is_admin()
+    async def restart(self, ctx):
+        log.info('Restarted Discord Bot ({}))'.format(ctx.message.author))
+        try:
+            os.execv(sys.executable, ['python'] + sys.argv)
+        except Exception as err:
+            log.warning('Failed to restart Discord Bot ({})'.format(err))
 
     @commands.command(hidden=True)
     @checks.is_admin()
