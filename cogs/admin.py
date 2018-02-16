f5f3f646c1c11fdd6bb4249d8a1041f20858820b
Tommy Ingdal <tommy.ingdal@gmail.com>
2018-02-16 21:29:54 +0100

    Tweety can now change/set nicknames.

--- cogs/admin.py
+++ cogs/admin.py
@@ -15,7 +15,15 @@ class Admin:
     def __init__(self, tweety):
         self.bot = tweety
 
-    @commands.command()
+    @commands.command(hidden=True)
+    @checks.is_admin()
+    async def nick(self, ctx, member: discord.Member, *, nickname: str=None):
+        try:
+            await member.edit(nick=nickname)
+        except discord.Forbidden:
+            log.critical('You do not have the proper permissions to change nicknames ({}).'.format(ctx.command))
+
+    @commands.command(hidden=True)
     @checks.is_admin()
     async def log(self, ctx, lines: int=5):
         with open('{}/tweety.log'.format(self.bot.base), 'r') as f:
@@ -25,38 +33,11 @@ class Admin:
             else:
                 await ctx.send('```[INFO] Log file is currently empty.```')
 
-    @commands.command()
-    async def source(self, ctx, command: str=None):
-        """Display the source code of a given command"""
-        source_url = 'https://github.com/s1gh/tweety'
-        if command is None:
-            await ctx.send(source_url)
-            return
-
-        obj = self.bot.get_command(command.replace('.', ' '))
-        src = obj.callback.__code__
-        lines, firstlineno = inspect.getsourcelines(src)
-
-        if not obj.callback.__module__.startswith('discord'):
-            # not a built-in command
-            location = os.path.relpath(src.co_filename).replace('\\', '/')
-        else:
-            location = obj.callback.__module__.replace('.', '/') + '.py'
-            source_url = 'https://github.com/Rapptz/discord.py'
-
-        final_url = '<{}/blob/master/{}#L{}-L{}>'.format(source_url, location, firstlineno,
-                                                         firstlineno + len(lines) - 1)
-
-        if len(lines) < 30:
-            lines = [x.replace('```', '´´´') for x in lines]
-            await ctx.send('**Source Code: {}**\n```python\n{}\n```'.format(command, ' '.join(lines)))
-        else:
-            await ctx.send(final_url)
-
     @commands.command(hidden=True)
     @checks.is_admin()
     async def restart(self, ctx):
         try:
+            await ctx.send('```[INFO] Restarting...```')
             log.info('Restarted Discord Bot ({}))'.format(ctx.message.author))
             os.execv(sys.executable, ['python'] + sys.argv)
         except Exception as err:
