f1598c5fcb842d4fb51e901476980537dff89c88
Tommy Ingdal <tommy.ingdal@gmail.com>
2018-02-24 20:10:52 +0100

    Can now display the most recent commit

--- cogs/info.py
+++ cogs/info.py
@@ -2,6 +2,8 @@ import discord
 import sys
 import os
 import inspect
+from config import github_access_token, github_repo_name
+from github import Github
 from pymongo import version as pymongo_version
 from discord.ext import commands
 from utils.misc import Embed, LinesOfCode, Uptime, Birthday
@@ -9,17 +11,17 @@ from utils.misc import Embed, LinesOfCode, Uptime, Birthday
 bot_info_thumb = 'https://s22.postimg.org/bgtc198pt/tweety_angry.png'
 python_icon = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/' \
               'Python-logo-notext.svg/2000px-Python-logo-notext.svg.png'
-embed_profile_icon = 'https://www.iconsdb.com/icons/preview/black/manager-xxl.png'
-embed_gaming_icon = 'https://cdn.iconscout.com/public/images/icon/free/png-512/' \
-                    'videogame-mushroom-toad-mario-bros-3cf1084383a9c2e6-512x512.png'
 
 class Info:
     def __init__(self, tweety):
         self.bot = tweety
+        self.github = Github(github_access_token).get_user().get_repo(github_repo_name)
 
     @commands.group()
     async def info(self, ctx):
         if ctx.invoked_subcommand is None:
+            last_git_sha_hash = self.github.get_commits(sha='master')[0].sha
+
             em = Embed()
             em.set_thumbnail(url=bot_info_thumb)
             em.set_author(name='Tweety', url='https://github.com/s1gh/tweety/')
@@ -29,9 +31,15 @@ class Info:
             em.add_field(name="Loaded Plugins", value=str(len(self.bot.extensions)))
             em.add_field(name="Developer", value="s1gh#9750")
             em.add_field(name="Birthday", value=Birthday(self.bot.user.created_at).get_birthday())
-            em.set_footer(text='Created with Python {} | Uptime: {}'.format(sys.version[:6], Uptime(self.bot.uptime).uptime()), icon_url=python_icon)
+            em.set_footer(text='Created with Python {} | Latest commit: {}'.format(sys.version[:6], last_git_sha_hash[:10]), icon_url=python_icon)
 
             await ctx.send(embed=em)
+
+    @info.command(name='commit')
+    async def last_commit(self, ctx):
+        commit = self.github.get_commits(sha='master')[0]
+
+        await ctx.send('```[{}] {}: {}```'.format(commit.commit.last_modified, commit.sha[:10], commit.commit.message))
 
     @info.command()
     async def uptime(self, ctx):
