ee533598bec6af5aff7766fafed7172f750b3a5a
s1gh <tommy.ingdal@gmail.com>
2018-03-04 21:38:24 +0100

    Database now includes a function for returning the database version

--- utils/database.py
+++ utils/database.py
@@ -8,6 +8,15 @@ class Database:
     def __init__(self, _pool):
         self.pool = _pool
 
+    async def get_db_version(self):
+        async with self.pool.acquire() as connection:
+            try:
+                version = await connection.fetch('SHOW SERVER_VERSION')
+                return [x for x in version[0].values()][0]
+            except Exception as err:
+                log.error(err)
+                return None
+
     @classmethod
     async def init_tables(cls, pool):
         async with pool.acquire() as connection:
