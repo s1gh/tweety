45d60e1c2c491b057853c9f04f57ade6774b57f6
s1gh <tommy.ingdal@gmail.com>
2018-04-01 23:15:44 +0200

    Changed description

--- cogs/tvmaze.py
+++ cogs/tvmaze.py
@@ -22,7 +22,7 @@ class Tvmaze:
 
     @commands.command(aliases=['ep'])
     async def nextepisode(self, ctx, *, show: str):
-        """Fetch information about the next episode of your favorite tv series"""
+        """Fetch information about the next episode of a given tv series"""
         async with self.bot.session.get(api_url.format(show)) as r:
             if r.status == 200:
                 ep = Episode(await r.json())
